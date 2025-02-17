555e624689ac92ad9e51bc1db7d1a1536a36170e
maxmiracle <vasiliev.maxim@gmail.com>
2025-02-18 02:57:56 +0300

    Initial commit

---
+++ src/main/java/ru/maximserver/pallas/PallasApplication.java
@@ -0,0 +1,13 @@
+package ru.maximserver.pallas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PallasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PallasApplication.class, args);
+	}
+
+}
